64edca31ab62f435766b30d0b39081c49a77d412
naman <namanddev7@gmail.com>
2025-05-27 03:45:43 +0530

    fix: remap search all files & use ts_ls

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
---[[init
+--[[initinit
 
 =====================================================================
 ==================== READ THIS BEFORE CONTINUING ====================
@@ -406,12 +406,13 @@ vim.keymap.set('n', '<leader>/', function()
 end, { desc = '[/] Fuzzily search in current buffer' })
 
 vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-vim.keymap.set('n', '<leader>sf', function()
+vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sF', function()
   require('telescope.builtin').find_files({
     no_ignore = true,
     hidden = true
   })
-end, { desc = '[S]earch [F]iles' })
+end, { desc = '[S]earch All [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
@@ -549,13 +550,6 @@ require('which-key').register {
 require('mason').setup()
 require('mason-lspconfig').setup()
 
--- Enable Formatters
-local mason = require 'mason';
-
-mason.setup {
-  ensure_installed = { "prettier" }
-}
-
 -- Enable the following language servers
 --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
 --
@@ -577,7 +571,7 @@ local servers = {
   purescriptls = {},
 
   rust_analyzer = {},
-  tsserver = {},
+  ts_ls = {},
   eslint = {},
   html = { filetypes = { 'html', 'twig', 'hbs' } },
 
